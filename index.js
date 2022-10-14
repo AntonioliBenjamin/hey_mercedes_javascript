64b93d2816a70ec47357ebf1b975334b791d3932
benjamin antonioli <benantonioli@outlook.fr>
2022-10-14 12:40:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+if (process.env.COMMAND === "weather") {
+    console.log("40° today");
+} else if (process.env.COMMAND === "address") {
+    console.log("241 chemin du bout de code");
+} else if (process.env.COMMAND === "quoteday") {
+    console.log("You see in this world there's two kinds of people, my friend. Those with loaded guns, and those who dig. You dig.");
+} else {
+    console.log("Désolé, je n'ai pas compris.");
+};
