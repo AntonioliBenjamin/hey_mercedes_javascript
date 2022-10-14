a228db89c3d64aa60bc0f9a300ee4d7df20bcc18
benjamin antonioli <benantonioli@outlook.fr>
2022-10-14 16:11:20 +0200

    command variable

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-if (process.env.COMMAND === "weather") {
+const command = process.env.COMMAND;
+
+if (command === "weather") {
     const weather = require("./weather");
     weather;
 
-} else if (process.env.COMMAND === "address") {
+} else if (command === "address") {
     const address = require("./address");
     address;
 
-} else if (process.env.COMMAND === "quoteday") {
+} else if (command === "quoteday") {
     const quoteday = require("./quoteday");
     quoteday;
 
